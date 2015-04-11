bb360bc42126082e4dc0206c884b7d41638426ed
Tien Yuan <tien.yuan@gmail.com>
2015-04-11 20:34:40 +0000

    adding static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+
   resources :adventures
 
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ spec/adventure_spec.rb
@@ -0,0 +1,6 @@
+require "spec_helper"
+require "adventure"
+
+describe Adventure do
+  it "has a title" # your examples (tests) go here
+end
